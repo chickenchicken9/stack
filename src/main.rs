df4ca0a6902fc9af2f680ab6434afd061559927f
Chicken C. Chicken <chickenchicken938427@gmail.com>
2023-04-27 15:27:21 +0200

    Handing over to GGRS now!

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,35 @@
 #![allow(clippy::too_many_arguments, clippy::type_complexity)]
 
 use bevy::{prelude::*, render::camera::ScalingMode, tasks::IoTaskPool};
-use bevy_matchbox::matchbox_socket::SingleChannel;
+use bevy_ggrs::*;
+use bevy_matchbox::matchbox_socket::{PeerId, SingleChannel, WebRtcSocket};
 use bevy_matchbox::MatchboxSocket;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                // fill the entire browser window
-                fit_canvas_to_parent: true,
-                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
-                prevent_default_event_handling: false,
-                ..default()
-            }),
+    let mut app = App::new();
+
+    GGRSPlugin::<GgrsConfig>::new()
+        .with_input_system(input)
+        .register_rollback_component::<Transform>()
+        .build(&mut app);
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            // fill the entire browser window
+            fit_canvas_to_parent: true,
+            // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+            prevent_default_event_handling: false,
             ..default()
-        }))
-        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
-        .add_startup_systems((setup, spawn_player, start_matchbox_socket))
-        .add_systems((move_player, wait_for_players))
-        .run();
+        }),
+        ..default()
+    }))
+    .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
+    .add_startup_systems((setup, spawn_player, start_matchbox_socket))
+    .add_systems((move_player.in_schedule(GGRSSchedule), wait_for_players))
+    .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {    
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut camera_bundle = Camera2dBundle::default();
     camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
     commands.spawn(camera_bundle);
@@ -62,7 +69,7 @@ fn start_matchbox_socket(mut commands: Commands) {
     commands.insert_resource(MatchboxSocket::new_ggrs(room_url));
 }
 
-fn wait_for_players(mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
+fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
     // Check for new connections
     socket.update_peers();
     let players = socket.players();
@@ -73,35 +80,106 @@ fn wait_for_players(mut socket: ResMut<MatchboxSocket<SingleChannel>>) {
     }
 
     info!("All peers have joined, going in-game");
-    // TODO
+
+    // create a GGRS P2P session
+    let mut session_builder = ggrs::SessionBuilder::<GgrsConfig>::new()
+        .with_num_players(num_players)
+        .with_input_delay(2);
+
+    for (i, player) in players.into_iter().enumerate() {
+        session_builder = session_builder
+            .add_player(player, i)
+            .expect("failed to add player");
+    }
+
+    // move the channel out of the socket (required because GGRS takes ownership of it)
+    if let Ok(channel) = socket.take_channel(0) {
+        // start the GGRS session
+        let ggrs_session = session_builder
+            .start_p2p_session(channel)
+            .expect("failed to start session");
+
+        commands.insert_resource(bevy_ggrs::Session::P2PSession(ggrs_session));
+    } else {
+        // channel was already taken, hopefully; how do i stop this from running?
+    }
 }
+
+struct GgrsConfig;
+
+impl ggrs::Config for GgrsConfig {
+    // 4-directions + fire fits easily in a single byte
+    type Input = u8;
+    type State = u8;
+    // Matchbox' WebRtcSocket addresses are called `PeerId`s
+    type Address = PeerId;
+}
+
+const INPUT_UP: u8 = 1 << 0;
+const INPUT_DOWN: u8 = 1 << 1;
+const INPUT_LEFT: u8 = 1 << 2;
+const INPUT_RIGHT: u8 = 1 << 3;
+const INPUT_FIRE: u8 = 1 << 4;
 
 #[derive(Component)]
 struct Player;
 
-fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((Player, SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0., 0.47, 1.),
-            custom_size: Some(Vec2::new(1., 1.)),
+fn spawn_player(mut commands: Commands, mut rip: ResMut<RollbackIdProvider>) {
+    commands.spawn((
+        Player,
+        rip.next(), // <-- NEW
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(-2., 0., 0.)),
+            sprite: Sprite {
+                color: Color::rgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
             ..default()
         },
-        ..default()
-    }));
+    ));
 }
 
-fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Transform, With<Player>>) {
-    let mut direction = Vec2::ZERO;
+fn input(_: In<ggrs::PlayerHandle>, keys: Res<Input<KeyCode>>) -> u8 {
+    let mut input = 0u8;
+
     if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
-        direction.y += 1.;
+        input |= INPUT_UP;
     }
     if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
-        direction.y -= 1.;
-    }
-    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
-        direction.x += 1.;
+        input |= INPUT_DOWN;
     }
     if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        input |= INPUT_LEFT
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        input |= INPUT_RIGHT;
+    }
+    if keys.any_pressed([KeyCode::Space, KeyCode::Return]) {
+        input |= INPUT_FIRE;
+    }
+
+    input
+}
+
+fn move_player(
+    inputs: Res<PlayerInputs<GgrsConfig>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+) {
+    let mut direction = Vec2::ZERO;
+
+    let (input, _) = inputs[0];
+
+    if input & INPUT_UP != 0 {
+        direction.y += 1.;
+    }
+    if input & INPUT_DOWN != 0 {
+        direction.y -= 1.;
+    }
+    if input & INPUT_RIGHT != 0 {
+        direction.x += 1.;
+    }
+    if input & INPUT_LEFT != 0 {
         direction.x -= 1.;
     }
     if direction == Vec2::ZERO {
